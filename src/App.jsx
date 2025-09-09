7ba61ebfc60838bf58d44437169a1fd66fdc8bb4
mukhammedaliametov <ametovmukhammedali07@gmail.com>
2025-09-09 20:19:29 +0500

    create project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-center font-bold underline text-3xl my-2'>Hello world</h1>
+    </div>
+  );
+};
+
+export default App;
