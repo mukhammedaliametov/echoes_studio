0d7d508a6bf0de0f186bf506e315ca3df4618b11
mukhammedaliametov <ametovmukhammedali07@gmail.com>
2025-09-13 05:57:18 +0500

    add plan.txt task.js

---
+++ src/task.js
@@ -0,0 +1,41 @@
+let son = -1; 
+if(son > 0){
+    console.log("musbat");
+} else if(son < 0){
+    console.log("manfiy");
+} else {
+    console.log("nol");
+}
+
+let yosh = 19;
+if(yosh >= 21){
+    console.log("Siz ulg‘aygansiz");
+} else {
+    console.log("Siz hali yoshsiz");
+}
+
+let raqam = 3;
+switch(raqam){
+    case 1: console.log("Dushanba"); break;
+    case 2: console.log("Seshanba"); break;
+    case 3: console.log("Chorshanba"); break;
+    case 4: console.log("Payshanba"); break;
+    case 5: console.log("Juma"); break;
+    case 6: console.log("Shanba"); break;
+    case 7: console.log("Yakshanba"); break;
+    default: console.log("Bunday kun yo‘q");
+}
+
+let t = 28;
+if(t > 30){
+    console.log("issiq");
+} else {
+    console.log("sovuq");
+}
+
+let num = 7;
+if(num % 2 === 0){
+    console.log(num * 3);
+} else {
+    console.log(num * 2);
+}
